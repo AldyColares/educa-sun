b68be25f867a4757f156c69bbc2d08e8e8da3738
aldycolares <aldycolares@gmail.com>
2021-07-21 10:23:03 -0300

    find in turma work

--- controller/turma.js
+++ controller/turma.js
@@ -1,41 +1,53 @@
 import database from '../services/conectdatabase.js'
-import filterObject from '../model/filters/objectfilter.js'
 import objectfilter from '../model/filters/objectfilter.js';
 let turmaController = {}
 
 turmaController.insertStudantInTurma = async function (req, res, next) {
-  const body = req.body,
-    databaseObject = await database.connect();
+  const body = req.body;
+  try {
+    const databaseObject = await database.connect();
 
-  await databaseObject.collection("turma")
-    .updateOne(
-      { name: body.nameturma },
-      {
-        $push: {
-          "listStudants": {
-            name: body.namestudant,
-            matriculation: body.matriculation,
-            age: body.age
+    await databaseObject.collection("turma")
+      .updateOne(
+        { name: body.nameturma },
+        {
+          $push: {
+            "listStudants": {
+              name: body.namestudant,
+              matriculation: body.matriculation,
+              age: body.age
+            }
           }
         }
-      }
-    );
-  return res.status(200).json({ message: "Registration studant in turma successful." });
+      );
+    return res.status(200).json({ message: "Registration studant in turma successful." });
 
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error internal serve." });
+  }
 }
 
 
 
 turmaController.listTurmasByStudant = async function (req, res, next) {
   const body = req.body;
-  const options = {
-    projection: { title: 1, imdb: 1 },
-  };
+
   try {
     const databaseObject = await database.connect();
-    const result = await databaseObject.collection("turma")
-      .find({ "listStudants.name": "carlos" });
-    return res.status(200).json({ result });
+    const cursor = await databaseObject.collection("turma")
+      .find({
+        "listStudants.matriculation": { matriculation: 2566 }
+
+        // "listStudants": {
+        // "$elemMatch": {
+        // "name": "pedro"
+        // }
+        // }
+      }).toArray();
+      
+    console.log(cursor);
+    return res.status(200).json({ cursor });
 
   } catch (error) {
     console.error(error);
@@ -56,6 +68,7 @@ turmaController.listStudandsByTurma = async function (req, res, next) {
       const resultFilter = objectfilter(body.filter, result.listStudants);
       return res.status(200).json({ resultFilter });
     }
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'error list studant by turma' });
